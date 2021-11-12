aa23973383809f3ef9f137962e49762193372202
Chiara <chiara.knipprath@iem.thm.de>
2021-11-12 13:21:01 +0100

    fab works too

--- app/src/main/java/de/thm/mow/felixwegener/simplydrive/MainActivity.kt
+++ app/src/main/java/de/thm/mow/felixwegener/simplydrive/MainActivity.kt
@@ -3,10 +3,14 @@ package de.thm.mow.felixwegener.simplydrive
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import de.thm.mow.felixwegener.simplydrive.databinding.ActivityMainBinding
@@ -19,9 +23,19 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    //Fragments
     private val homeFragment = HomeFragment()
     private val settingsFragment = SettingsFragment()
     private val historyFragment = HistoryFragment()
+
+    //FAB Button(s)
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim) }
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim) }
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim) }
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim) }
+    //private val icAdd =
+
+    private var clicked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -32,6 +46,8 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.background = null
         bottomNavigationView.menu.getItem(4).isEnabled = false
 
+
+        //Fragments
         replaceFragment(homeFragment)
 
         bottomNavigationView.setOnNavigationItemSelectedListener {
@@ -42,13 +58,64 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+        //FAB Button(s)
+        fab_main.setOnClickListener {
+            onAddButtonClicked()
+        }
+        fab_search.setOnClickListener {
+            Toast.makeText(this, "Search Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+        fab_edit.setOnClickListener {
+            Toast.makeText(this, "Edit Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
+
 
     private fun replaceFragment(fragment: Fragment) {
         if (fragment != null) {
             val fragmentTransaction =  supportFragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.fragmentContainer, fragment, "history_fragment")
             fragmentTransaction.commit()
+        }
+    }
+
+    private fun onAddButtonClicked() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        setClickable(clicked)
+        clicked = !clicked
+    }
+
+    private fun setVisibility(clicked: Boolean) {
+        if(!clicked){
+            fab_search.visibility = View.VISIBLE
+            fab_edit.visibility = View.VISIBLE
+        } else {
+            fab_search.visibility = View.INVISIBLE
+            fab_edit.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setAnimation(clicked: Boolean) {
+        if(!clicked){
+            fab_edit.startAnimation(fromBottom)
+            fab_search.startAnimation(fromBottom)
+        } else {
+            fab_edit.startAnimation(toBottom)
+            fab_search.startAnimation(toBottom)
+        }
+    }
+
+    private fun setClickable (clicked: Boolean){
+        if(!clicked){
+            fab_search.isClickable= true
+            fab_edit.isClickable= true
+        }else{
+            fab_search.isClickable= false
+            fab_edit.isClickable= false
         }
     }
 
