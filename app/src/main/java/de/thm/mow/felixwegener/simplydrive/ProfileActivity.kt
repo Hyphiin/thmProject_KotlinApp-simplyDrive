9edb7655927afa1fbed4c6cc1cb0e78dcb68b4c3
Felix Wegener <felix.wegener@iem.thm.de>
2021-11-13 17:39:32 +0100

    wip

--- app/src/main/java/de/thm/mow/felixwegener/simplydrive/ProfileActivity.kt
+++ app/src/main/java/de/thm/mow/felixwegener/simplydrive/ProfileActivity.kt
@@ -5,32 +5,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import de.thm.mow.felixwegener.simplydrive.databinding.ActivityProfileBinding
 import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.nav_header.*
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        val userId = intent.getStringExtra("user_id")
-        val emailId = intent.getStringExtra("email_id")
-
-        Log.d("----------------", "$userId")
-
-        user_id__View.text = "User ID :: $userId"
-        user_email__View.text = "Email ID :: $emailId"
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
 
         logOut__btn.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-
-            startActivity(Intent(this@ProfileActivity, LoginActivity::class.java))
-            finish()
+            firebaseAuth.signOut()
+            checkUser()
         }
 
         tv__MainActivity.setOnClickListener {
             startActivity(Intent(this@ProfileActivity, MainActivity::class.java))
             finish()
+        }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null) {
+            startActivity(Intent(this@ProfileActivity, LoginActivity::class.java))
+            finish()
+        }
+        else {
+            val email = firebaseUser.email
+            val uid = firebaseUser.uid
+
+            user_id__View.text = "User ID :: $uid"
+            user_email__View.text = "Email ID :: $email"
         }
     }
 }
--- app/src/main/java/de/thm/mow/felixwegener/simplydrive/RegisterActivity.kt
+++ app/src/main/java/de/thm/mow/felixwegener/simplydrive/RegisterActivity.kt
@@ -1,19 +1,34 @@
 package de.thm.mow.felixwegener.simplydrive
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private companion object{
+        private const val RC__Sign__IN = 100
+        private const val Tag = "Google_Sign_In-Tag"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -39,11 +54,7 @@ class RegisterActivity : AppCompatActivity() {
 
                                     Toast.makeText(this@RegisterActivity, "Erfolgreich registriert!", Toast.LENGTH_SHORT).show()
 
-                                    val intent = Intent(this@RegisterActivity, ProfileActivity::class.java)
-                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                    intent.putExtra("user_id", firebaseUser.uid)
-                                    intent.putExtra("email_id", email)
-                                    startActivity(intent)
+                                    startActivity(Intent(this@RegisterActivity, ProfileActivity::class.java))
                                     finish()
                                 } else {
                                     Toast.makeText(this@RegisterActivity, task.exception!!.message.toString(), Toast.LENGTH_SHORT).show()
@@ -54,18 +65,82 @@ class RegisterActivity : AppCompatActivity() {
             }
         }
 
-        register__google.setOnClickListener {
-            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build()
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("957162577294-eh1nplorecq33hgoi7amsja4mp1qlhru.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
 
-            //Build a GoogleSignInClient with the options specified by gso.
-            val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        register__google.setOnClickListener {
+            val intent = googleSignInClient.signInIntent
+            startActivityForResult(intent, RC__Sign__IN)
+
         }
 
         registerLoginField.setOnClickListener {
             startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
             finish()
         }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null) {
+            startActivity(Intent(this@RegisterActivity, ProfileActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC__Sign__IN) {
+            val accountTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = accountTask.getResult(ApiException::class.java)
+                firebaseAuthWithGoogleAccount(account)
+            }
+            catch (e: Exception) {
+                Log.d(TAG, "OnActivityResult: ${e.message}")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogleAccount(account: GoogleSignInAccount?) {
+        Log.d(TAG, "firebaseAuthWithGoogleAccount: begin firebase auth with google account")
+
+        val credential = GoogleAuthProvider.getCredential(account!!.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnSuccessListener { authResult ->
+
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: LoggedIn!")
+
+                val firebaseUser = firebaseAuth.currentUser
+
+                val uid = firebaseUser!!.uid
+                val email = firebaseUser!!.email
+
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: Uid: $uid")
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: Email: $email")
+
+                if (authResult.additionalUserInfo!!.isNewUser) {
+                    Log.d(TAG, "firebaseAuthWithGoogleAccount: Account created... n\$email")
+                    Toast.makeText(this@RegisterActivity, "Account created... n$email", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.d(TAG, "firebaseAuthWithGoogleAccount: Existing user... n\$email")
+                    Toast.makeText(this@RegisterActivity, "Welcome back... n$email", Toast.LENGTH_SHORT).show()
+                }
+
+                //start ProfileActivity
+                startActivity(Intent(this@RegisterActivity, ProfileActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener{ e ->
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: Login failed due to ${e.message}")
+                Toast.makeText(this@RegisterActivity, "Login failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
