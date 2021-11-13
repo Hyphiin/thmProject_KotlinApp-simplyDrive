574bf58ea7c57c19fa24fc352bfa6d1451719f8d
Felix Wegener <felix.wegener@iem.thm.de>
2021-11-13 15:06:49 +0100

    new Version

--- app/src/main/java/de/thm/mow/felixwegener/simplydrive/MainActivity.kt
+++ app/src/main/java/de/thm/mow/felixwegener/simplydrive/MainActivity.kt
@@ -1,173 +1,146 @@
-package de.thm.mow.felixwegener.simplydrive
+    package de.thm.mow.felixwegener.simplydrive
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
-import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
-import de.thm.mow.felixwegener.simplydrive.databinding.ActivityMainBinding
-import de.thm.mow.felixwegener.simplydrive.fragments.*
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_profile.*
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import android.view.View
+    import android.view.animation.Animation
+    import android.view.animation.AnimationUtils
+    import androidx.fragment.app.Fragment
+    import de.thm.mow.felixwegener.simplydrive.databinding.ActivityMainBinding
+    import de.thm.mow.felixwegener.simplydrive.fragments.*
+    import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    class MainActivity : AppCompatActivity() {
 
-    //Fragments
-    private val homeFragment = HomeFragment()
-    private val settingsFragment = SettingsFragment()
-    private val historyFragment = HistoryFragment()
-    private val editFragment = EditFragment()
-    private val scanFragment = ScanFragment()
-
-    //FAB Button(s)
-    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim) }
-    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim) }
-    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim) }
-    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim) }
-    //private val icAdd =
-
-    private var clicked = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        bottomNavigationView.background = null
-        bottomNavigationView.menu.getItem(4).isEnabled = false
-
+        private lateinit var binding: ActivityMainBinding
 
         //Fragments
-        replaceFragment(homeFragment)
-
-        bottomNavigationView.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.nav_home -> replaceFragment(homeFragment)
-                R.id.nav_setting -> replaceFragment(settingsFragment)
-                R.id.nav_history -> replaceFragment(historyFragment)
-            }
-            true
-        }
+        private val homeFragment = HomeFragment()
+        private val settingsFragment = SettingsFragment()
+        private val historyFragment = HistoryFragment()
+        private val editFragment = EditFragment()
+        private val scanFragment = ScanFragment()
 
         //FAB Button(s)
-        fab_main.setOnClickListener {
-            onAddButtonClicked()
-        }
-        fab_scan.setOnClickListener {
-            replaceFragment(scanFragment)
-        }
-        fab_edit.setOnClickListener {
-            replaceFragment(editFragment)
+        private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim) }
+        private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim) }
+        private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim) }
+        private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim) }
+        //private val icAdd =
+
+        private var clicked = false
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            bottomNavigationView.background = null
+            bottomNavigationView.menu.getItem(4).isEnabled = false
+
+
+            //Fragments
+            replaceFragment(homeFragment)
+
+            bottomNavigationView.setOnNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.nav_home -> replaceFragment(homeFragment)
+                    R.id.nav_setting -> replaceFragment(settingsFragment)
+                    R.id.nav_history -> replaceFragment(historyFragment)
+                }
+                true
+            }
+
+            //FAB Button(s)
+            fab_main.setOnClickListener {
+                onAddButtonClicked()
+            }
+            fab_scan.setOnClickListener {
+                replaceFragment(scanFragment)
+            }
+            fab_edit.setOnClickListener {
+                replaceFragment(editFragment)
+            }
+
+
         }
 
+
+        private fun replaceFragment(fragment: Fragment) {
+            if (fragment != null) {
+                val fragmentTransaction =  supportFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.fragmentContainer, fragment, "history_fragment")
+                fragmentTransaction.commit()
+            }
+
+            /*
+            user_LogoutBtn.setOnClickListener {
+                FirebaseAuth.getInstance().signOut()
+
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+                finish()
+            }*/
+
+            /*
+            val user = FirebaseAuth.getInstance().currentUser
+            user?.let {
+                // Name, email address, and profile photo Url
+                val name = user.displayName
+                val email = user.email
+                val photoUrl = user.photoUrl
+
+                // Check if user's email is verified
+                val emailVerified = user.isEmailVerified
+
+                // The user's ID, unique to the Firebase project. Do NOT use this value to
+                // authenticate with your backend server, if you have one. Use
+                // FirebaseUser.getToken() instead.
+                val uid = user.uid
+
+                home__uMail.text = "Willkommen ${email} !"
+
+                Log.d("uid", uid)
+
+            }*/
+        }
+
+        private fun onAddButtonClicked() {
+            setVisibility(clicked)
+            setAnimation(clicked)
+            setClickable(clicked)
+            clicked = !clicked
+        }
+
+        private fun setVisibility(clicked: Boolean) {
+            if(!clicked){
+                fab_scan.visibility = View.VISIBLE
+                fab_edit.visibility = View.VISIBLE
+            } else {
+                fab_scan.visibility = View.INVISIBLE
+                fab_edit.visibility = View.INVISIBLE
+            }
+        }
+
+         private fun setAnimation(clicked: Boolean) {
+            if(!clicked){
+                fab_edit.startAnimation(fromBottom)
+                fab_scan.startAnimation(fromBottom)
+            } else {
+                fab_edit.startAnimation(toBottom)
+                fab_scan.startAnimation(toBottom)
+            }
+         }
+
+        private fun setClickable (clicked: Boolean){
+            if(!clicked){
+                fab_scan.isClickable= true
+                fab_edit.isClickable= true
+            }else{
+                fab_scan.isClickable= false
+                fab_edit.isClickable= false
+            }
+        }
 
     }
-
-
-    private fun replaceFragment(fragment: Fragment) {
-        if (fragment != null) {
-            val fragmentTransaction =  supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.fragmentContainer, fragment, "history_fragment")
-            fragmentTransaction.commit()
-        }
-
-        user_LogoutBtn.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-
-            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
-            finish()
-        }
-
-        val user = FirebaseAuth.getInstance().currentUser
-        user?.let {
-            // Name, email address, and profile photo Url
-            val name = user.displayName
-            val email = user.email
-            val photoUrl = user.photoUrl
-
-            // Check if user's email is verified
-            val emailVerified = user.isEmailVerified
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getToken() instead.
-            val uid = user.uid
-
-            home__uMail.text = "Willkommen ${email} !"
-
-            Log.d("uid", uid)
-
-        }
-    }
-
-    private fun onAddButtonClicked() {
-        setVisibility(clicked)
-        setAnimation(clicked)
-        setClickable(clicked)
-        clicked = !clicked
-    }
-
-    private fun setVisibility(clicked: Boolean) {
-        if(!clicked){
-            fab_scan.visibility = View.VISIBLE
-            fab_edit.visibility = View.VISIBLE
-        } else {
-            fab_scan.visibility = View.INVISIBLE
-            fab_edit.visibility = View.INVISIBLE
-        }
-    }
-
-    private fun addDBEntry() {
-
-        val user = FirebaseAuth.getInstance().currentUser
-        user?.let {
-            val uid = user.uid
-
-            dateInput = findViewById<View>(R.id.DateInput) as EditText
-            timeInput = findViewById<View>(R.id.TimeInput) as EditText
-            routeInput = findViewById<View>(R.id.RouteInput) as EditText
-
-            val date = dateInput.text.toString()
-            val time = timeInput.text.toString()
-            val route = routeInput.text.toString()
-
-            val newRoute = Route(date, time, route, uid)
-            latHisAdapter.addHistory(newRoute)
-        }
-    }
-
-     private fun setAnimation(clicked: Boolean) {
-        if(!clicked){
-            fab_edit.startAnimation(fromBottom)
-            fab_scan.startAnimation(fromBottom)
-        } else {
-            fab_edit.startAnimation(toBottom)
-            fab_scan.startAnimation(toBottom)
-        }
-     }    
-
-    private fun setClickable (clicked: Boolean){
-        if(!clicked){
-            fab_scan.isClickable= true
-            fab_edit.isClickable= true
-        }else{
-            fab_scan.isClickable= false
-            fab_edit.isClickable= false
-        }
-    }
-
-}
--- app/src/main/java/de/thm/mow/felixwegener/simplydrive/fragments/HistoryFragment.kt
+++ app/src/main/java/de/thm/mow/felixwegener/simplydrive/fragments/HistoryFragment.kt
@@ -1,81 +1,86 @@
-package de.thm.mow.felixwegener.simplydrive.fragments
+    package de.thm.mow.felixwegener.simplydrive.fragments
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import de.thm.mow.felixwegener.simplydrive.LatHisAdapter
-import de.thm.mow.felixwegener.simplydrive.R
-import de.thm.mow.felixwegener.simplydrive.Route
-import kotlinx.android.synthetic.main.fragment_history.view.*
+    import android.os.Bundle
+    import android.view.LayoutInflater
+    import android.view.View
+    import android.view.ViewGroup
+    import android.widget.EditText
+    import androidx.fragment.app.Fragment
+    import androidx.recyclerview.widget.LinearLayoutManager
+    import androidx.recyclerview.widget.RecyclerView
+    import com.google.firebase.auth.FirebaseAuth
+    import de.thm.mow.felixwegener.simplydrive.LatHisAdapter
+    import de.thm.mow.felixwegener.simplydrive.R
+    import de.thm.mow.felixwegener.simplydrive.Route
+    import kotlinx.android.synthetic.main.fragment_history.view.*
 
-class HistoryFragment :Fragment(R.layout.fragment_history), LatHisAdapter.ClickListener {
+    class HistoryFragment :Fragment(R.layout.fragment_history), LatHisAdapter.ClickListener {
 
-    private lateinit var latHisAdapter: LatHisAdapter
+        private lateinit var latHisAdapter: LatHisAdapter
 
-    lateinit var dateInput: EditText
-    lateinit var timeInput: EditText
-    lateinit var routeInput: EditText
+        lateinit var dateInput: EditText
+        lateinit var timeInput: EditText
+        lateinit var routeInput: EditText
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_history, container, false)
-        initRecyclerView(view)
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val view = inflater.inflate(R.layout.fragment_history, container, false)
+            initRecyclerView(view)
 
-        view.bShowHistory.setOnClickListener { view ->
-            addDBEntry()
+            view.bShowHistory.setOnClickListener { view ->
+                addDBEntry()
+            }
+            view.clearHistory.setOnClickListener { view ->
+                clearDB()
+            }
+
+            return view
         }
-        view.clearHistory.setOnClickListener { view ->
-            clearDB()
+
+
+
+        private fun initRecyclerView(view: View) {
+            val recyclerView = view.findViewById<RecyclerView>(R.id.rvLatestHistory)
+            recyclerView.layoutManager = LinearLayoutManager(activity)
+            latHisAdapter = LatHisAdapter(mutableListOf(), this)
+            recyclerView.adapter = latHisAdapter
         }
 
-        return view
-    }
 
 
+        private fun addDBEntry() {
 
-    private fun initRecyclerView(view: View) {
-        val recyclerView = view.findViewById<RecyclerView>(R.id.rvLatestHistory)
-        recyclerView.layoutManager = LinearLayoutManager(activity)
-        latHisAdapter = LatHisAdapter(mutableListOf(), this)
-        recyclerView.adapter = latHisAdapter
-    }
+            val user = FirebaseAuth.getInstance().currentUser
+            user?.let {
+                val uid = user.uid
 
+            dateInput = view?.findViewById(R.id.DateInput) as EditText
+            timeInput = view?.findViewById(R.id.TimeInput) as EditText
+            routeInput = view?.findViewById(R.id.RouteInput) as EditText
 
+            val date = dateInput.text.toString()
+            val time = timeInput.text.toString()
+            val route = routeInput.text.toString()
 
-    private fun addDBEntry() {
+            val newRoute = Route(date, time, route, uid)
+            latHisAdapter.addHistory(newRoute)}
+        }
 
-        dateInput = view?.findViewById(R.id.DateInput) as EditText
-        timeInput = view?.findViewById(R.id.TimeInput) as EditText
-        routeInput = view?.findViewById(R.id.RouteInput) as EditText
+        private fun clearDB() {
+            latHisAdapter.clearDB()
+        }
 
-        val date = dateInput.text.toString()
-        val time = timeInput.text.toString()
-        val route = routeInput.text.toString()
+        override fun onItemClick(route: Route) {
+           val fragment: Fragment = HistoryDetailView.newInstance(route.route!!)
+           val transaction = activity?.supportFragmentManager!!.beginTransaction()
+           transaction.hide(activity?.supportFragmentManager!!.findFragmentByTag("history_fragment")!!)
+           transaction.add(R.id.fragmentContainer, fragment)
+           transaction.addToBackStack(null)
+           transaction.commit()
 
-        val newRoute = Route(date, time, route)
-        latHisAdapter.addHistory(newRoute)
-    }
-
-    private fun clearDB() {
-        latHisAdapter.clearDB()
-    }
-
-    override fun onItemClick(route: Route) {
-       val fragment: Fragment = HistoryDetailView.newInstance(route.route!!)
-       val transaction = activity?.supportFragmentManager!!.beginTransaction()
-       transaction.hide(activity?.supportFragmentManager!!.findFragmentByTag("history_fragment")!!)
-       transaction.add(R.id.fragmentContainer, fragment)
-       transaction.addToBackStack(null)
-       transaction.commit()
+        }
 
     }
-
-}
