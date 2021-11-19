1b52fdc304305b6c4fbf316c74385f8da4d49557
Felix Wegener <felix.wegener@iem.thm.de>
2021-11-19 19:39:47 +0100

    new Way to Add routes
    Routes sind jetzt geordnet

--- app/src/main/java/de/thm/mow/felixwegener/simplydrive/Route.kt
+++ app/src/main/java/de/thm/mow/felixwegener/simplydrive/Route.kt
@@ -1,6 +1,6 @@
 package de.thm.mow.felixwegener.simplydrive
 
-data class Route(var date: String? = null, var time: String? = null, var route: String? = null, var uid: String? = null)
+data class Route(var date: String? = null, var time: String? = null, var start: String? = null, var end: String? = null, var line: String? = null, var uid: String? = null)
 
 //https://firebase.google.com/docs/database/android/read-and-write#get_a_databasereference
 
--- app/src/main/java/de/thm/mow/felixwegener/simplydrive/fragments/HomeFragment.kt
+++ app/src/main/java/de/thm/mow/felixwegener/simplydrive/fragments/HomeFragment.kt
@@ -1,15 +1,27 @@
 package de.thm.mow.felixwegener.simplydrive.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.*
+import de.thm.mow.felixwegener.simplydrive.LatHisAdapter
 import de.thm.mow.felixwegener.simplydrive.R
+import de.thm.mow.felixwegener.simplydrive.Route
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
 class HomeFragment : Fragment() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var currentUserID: String
+
+    private lateinit var databaseRef: FirebaseFirestore
+    private lateinit var routesArrayList: MutableList<Route>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,6 +50,39 @@ class HomeFragment : Fragment() {
         transaction.replace(R.id.fragmentContainer, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+    private fun getUserData(): MutableList<Route> {
+        routesArrayList = mutableListOf()
+        databaseRef = FirebaseFirestore.getInstance()
+
+        mAuth = FirebaseAuth.getInstance()
+        val firebaseUser = mAuth.currentUser
+
+        if (firebaseUser != null) {
+            currentUserID = firebaseUser.uid
+        }
+
+        databaseRef.collection("routes").whereEqualTo("uid", currentUserID).orderBy("date", Query.Direction.DESCENDING).limit(3)
+            .addSnapshotListener(object : EventListener<QuerySnapshot> {
+                override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                    if (error != null) {
+                        Log.e("Firestore Error", error.message.toString())
+                        return
+                    }
+
+                    for (dc: DocumentChange in value?.documentChanges!!){
+                        if (dc.type == DocumentChange.Type.ADDED) {
+                            routesArrayList.add(dc.document.toObject(Route::class.java))
+                        }
+                    }
+                    Log.d("==========>", routesArrayList.toString())
+
+                }
+            })
+
+        return routesArrayList
+
     }
 
 }
