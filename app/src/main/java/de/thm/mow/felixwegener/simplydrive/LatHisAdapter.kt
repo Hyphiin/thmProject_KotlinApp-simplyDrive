f8519c3dce5beed6cae2ec260678d407ac4979b7
Felix Wegener <felix.wegener@iem.thm.de>
2021-12-10 12:07:33 +0100

    Wip

--- app/src/main/java/de/thm/mow/felixwegener/simplydrive/LatHisAdapter.kt
+++ app/src/main/java/de/thm/mow/felixwegener/simplydrive/LatHisAdapter.kt
@@ -1,17 +1,29 @@
 package de.thm.mow.felixwegener.simplydrive
 
 
+import android.app.Dialog
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import java.io.FileOutputStream
+import java.lang.Exception
 
 
 class LatHisAdapter(private val routesList: MutableList<Route>) :
     RecyclerView.Adapter<LatHisAdapter.LatHisViewHolder>() {
 
+    private lateinit var tvDatePopUp: TextView
+    private lateinit var tvTimePopUp: TextView
+    private lateinit var tvDeparturePopUp: TextView
+    private lateinit var tvDestinationPopUp: TextView
+    private lateinit var tvLinePopUp: TextView
+    private lateinit var exportBtn: Button
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LatHisViewHolder {
 
@@ -20,7 +32,33 @@ class LatHisAdapter(private val routesList: MutableList<Route>) :
         val itemView =
             LayoutInflater.from(parent.context).inflate(R.layout.item_latest_history, parent, false)
 
-        return LatHisViewHolder(itemView)
+        val myDialog = Dialog(parent.context)
+        myDialog.setContentView(R.layout.dialog__drive)
+
+        tvDatePopUp = myDialog.findViewById(R.id.tvLHdatePopUp)
+        tvTimePopUp = myDialog.findViewById(R.id.tvLHtimePopUp)
+        tvDeparturePopUp = myDialog.findViewById(R.id.tvLHrouteStartPopUp)
+        tvDestinationPopUp = myDialog.findViewById(R.id.tvLHrouteEndPopUp)
+        tvLinePopUp = myDialog.findViewById(R.id.tvLHrouteLinePopUp)
+        exportBtn = myDialog.findViewById(R.id.btn__export)
+
+        exportBtn.setOnClickListener {
+            Toast.makeText(parent.context, "Export!", Toast.LENGTH_SHORT).show()
+        }
+
+        return LatHisViewHolder(itemView).listen { pos, _ ->
+            val item = routesList[pos]
+            //TODO do other stuff here
+            Log.d("HEEEEEELLLLLLLOOOO", item.toString())
+
+            tvDatePopUp.text = routesList[pos].date.toString()
+            tvTimePopUp.text = routesList[pos].time
+            tvDeparturePopUp.text = routesList[pos].start
+            tvDestinationPopUp.text = routesList[pos].end
+            tvLinePopUp.text = routesList[pos].line
+
+            myDialog.show()
+        }
     }
 
     override fun onBindViewHolder(holder: LatHisViewHolder, position: Int) {
@@ -48,5 +86,12 @@ class LatHisAdapter(private val routesList: MutableList<Route>) :
         val tvlHrouteEnd: TextView = itemView.findViewById(R.id.tvLHrouteEnd)
         val tvlHrouteLine: TextView = itemView.findViewById(R.id.tvLHrouteLine)
 
+    }
+
+    fun <T : RecyclerView.ViewHolder> T.listen(event: (position: Int, type: Int) -> Unit): T {
+        itemView.setOnClickListener {
+            event.invoke(getAdapterPosition(), itemViewType)
+        }
+        return this
     }
 }
